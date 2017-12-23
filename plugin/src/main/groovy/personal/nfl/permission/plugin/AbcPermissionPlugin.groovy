1cacb4f922c6d88e370c7f06feffa26a7534519e
choochoofly <2017398956@qq.com>
2017-12-23 16:23:53 +0800

    update

---
+++ plugin/src/main/groovy/personal/nfl/permission/plugin/AbcPermissionPlugin.groovy
@@ -0,0 +1,46 @@
+package personal.nfl.permission.plugin
+
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
+import org.aspectj.tools.ajc.Main
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+
+class AbcPermissionPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.android.applicationVariants.all { variant ->
+            Task javaCompile = variant.getJavaCompiler()
+            javaCompile.doLast {
+                String[] args = ["-showWeaveInfo",
+                                 "-1.5",
+                                 "-inpath", javaCompile.destinationDir.toString(),
+                                 "-aspectpath", javaCompile.classpath.asPath,
+                                 "-d", javaCompile.destinationDir.toString(),
+                                 "-classpath", javaCompile.classpath.asPath,
+                                 "-bootclasspath", android.bootClasspath.join(File.pathSeparator)]
+                MessageHandler handler = new MessageHandler(true)
+                new Main().run(args, handler)
+
+                def log = project.logger
+                for (IMessage message : handler.getMessages(null, true)) {
+                    switch (message.getKind()) {
+                        case IMessage.ABORT:
+                        case IMessage.ERROR:
+                        case IMessage.FAIL:
+                            log.error message.message, message.thrown
+                            break;
+                        case IMessage.WARNING:
+                        case IMessage.INFO:
+                            log.info message.message, message.thrown
+                            break;
+                        case IMessage.DEBUG:
+                            log.debug message.message, message.thrown
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
