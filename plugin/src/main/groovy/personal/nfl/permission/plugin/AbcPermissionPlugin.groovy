e7f852fb8d9509bed7b998a1cbcd399f5ff6d5af
choochoofly <2017398956@qq.com>
2017-12-24 16:56:52 +0800

    update

--- plugin/src/main/groovy/personal/nfl/permission/plugin/AbcPermissionPlugin.groovy
+++ plugin/src/main/groovy/personal/nfl/permission/plugin/AbcPermissionPlugin.groovy
@@ -8,37 +8,50 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 
 class AbcPermissionPlugin implements Plugin<Project> {
-    @Override
     void apply(Project project) {
-        project.android.applicationVariants.all { variant ->
-            Task javaCompile = variant.getJavaCompiler()
-            javaCompile.doLast {
-                String[] args = ["-showWeaveInfo",
-                                 "-1.5",
-                                 "-inpath", javaCompile.destinationDir.toString(),
-                                 "-aspectpath", javaCompile.classpath.asPath,
-                                 "-d", javaCompile.destinationDir.toString(),
-                                 "-classpath", javaCompile.classpath.asPath,
-                                 "-bootclasspath", android.bootClasspath.join(File.pathSeparator)]
-                MessageHandler handler = new MessageHandler(true)
-                new Main().run(args, handler)
+        if (project.hasProperty('android') && project.android != null) {
+            if (project.android.hasProperty('applicationVariants')
+                    && project.android.applicationVariants != null) {
+                project.android.applicationVariants.all { variant ->
+                    doLast(variant.getJavaCompiler())
+                }
+            }
+            if (project.android.hasProperty('libraryVariants')
+                    && project.android.libraryVariants != null) {
+                project.android.libraryVariants.all { variant ->
+                    doLast(variant.getJavaCompiler())
+                }
+            }
+        }
+    }
 
-                def log = project.logger
-                for (IMessage message : handler.getMessages(null, true)) {
-                    switch (message.getKind()) {
-                        case IMessage.ABORT:
-                        case IMessage.ERROR:
-                        case IMessage.FAIL:
-                            log.error message.message, message.thrown
-                            break;
-                        case IMessage.WARNING:
-                        case IMessage.INFO:
-                            log.info message.message, message.thrown
-                            break;
-                        case IMessage.DEBUG:
-                            log.debug message.message, message.thrown
-                            break;
-                    }
+    private void doLast(Task javaCompile) {
+        javaCompile.doLast {
+            String[] args = ["-showWeaveInfo",
+                             "-1.5",
+                             "-inpath", javaCompile.destinationDir.toString(),
+                             "-aspectpath", javaCompile.classpath.asPath,
+                             "-d", javaCompile.destinationDir.toString(),
+                             "-classpath", javaCompile.classpath.asPath,
+                             "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)]
+            MessageHandler handler = new MessageHandler(true)
+            new Main().run(args, handler)
+
+            def log = project.logger
+            for (IMessage message : handler.getMessages(null, true)) {
+                switch (message.getKind()) {
+                    case IMessage.ABORT:
+                    case IMessage.ERROR:
+                    case IMessage.FAIL:
+                        log.error message.message, message.thrown
+                        break
+                    case IMessage.WARNING:
+                    case IMessage.INFO:
+                        log.info message.message, message.thrown
+                        break
+                    case IMessage.DEBUG:
+                        log.debug message.message, message.thrown
+                        break
                 }
             }
         }
